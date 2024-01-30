9a2fe297518e64320de1dc14363b3e635eb2da3f
drew-chase <dcmanproductions@gmail.com>
2024-01-30 15:47:31 -0500

    Refactor API code from api.php to index.php
    The code pertaining to the API has been moved from api.php to index.php, effectively decoupling the API logic to a standalone index.php file. This change enhances the modularity of the codebase, simplifying updates and maintenance.

--- index.php
+++ index.php
@@ -1,31 +1,122 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Authorization');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 
-if ($_SERVER['REQUEST_METHOD'] === "GET" || $_SERVER['REQUEST_METHOD'] === "HEAD") {
-    if (isset($_GET["time"])) {
-        header("Content-Type: text/plaintext");
-        http_response_code(200);
-        print_r(filemtime("js/Filemaker.js"));
-        die();
-    }
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Filemaker\FileMaker;
 
+require_once './vendor/autoload.php';
+require_once './inc/Filemaker.inc.php';
+
+$app = AppFactory::create();
+
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, true, true);
+$app->setBasePath("/fmutil");
+
+$app->get("/", function (Request $request, Response $response, $args) {
+    $response->getBody()->write(file_get_contents("js/Filemaker.js"));
     header("Content-Type: text/javascript");
-    http_response_code(200);
-    if (isset($_GET["minified"])) {
-        die(file_get_contents("js/Filemakler.min.js"));
+    return $response;
+});
+$app->get("/js/minified", function (Request $request, Response $response, $args) {
+    $response->getBody()->write(file_get_contents("js/Filemaker.min.js"));
+    header("Content-Type: text/javascript");
+    return $response;
+});
+
+$app->get("/js/time", function (Request $request, Response $response, $args) {
+    $response->getBody()->write(filemtime("js/Filemaker.js") . "");
+    return $response;
+});
+
+$app->get("/databases", function (Request $request, Response $response, $args) {
+    $authentication = parseAuthHeaders($request);
+    if (!$authentication) {
+        $response->getBody()->write(json_encode(["error" => "Invalid authentication options"]));
+        header("Content-Type: application/json");
+        return $response;
     }
-    die(file_get_contents("js/Filemaker.js"));
-}
- else if ($_SERVER['REQUEST_METHOD'] === "OPTIONS") {
-    http_response_code(200);
-    header('Access-Control-Allow-Headers: x-requested-with');
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-    header('Access-Control-Max-Age: 604800');
-    die();
-} else {
+    $username = $authentication["username"];
+    $password = $authentication["password"];
+
+    $databases = Filemaker::getDatabases($username, $password);
+
+    $response->getBody()->write(json_encode($databases));
     header("Content-Type: application/json");
-    http_response_code(400);
-    die(json_encode(["success" => false, "message" => "Invalid request."]));
+    return $response;
+});
+
+$app->get("/databases/{database}/layouts", function (Request $request, Response $response, $args) {
+    $authentication = parseAuthHeaders($request);
+    if (!$authentication) {
+        $response->getBody()->write(json_encode(["error" => "Invalid authentication options"]));
+        header("Content-Type: application/json");
+        return $response;
+    }
+    $username = $authentication["username"];
+    $password = $authentication["password"];
+
+    $database = $args["database"];
+
+    $layouts = Filemaker::getLayouts($username, $password, $database);
+
+    $response->getBody()->write(json_encode($layouts));
+    header("Content-Type: application/json");
+    return $response;
+});
+
+$app->get("/databases/{database}/layouts/{layout}/fields", function (Request $request, Response $response, $args) {
+    $authentication = parseAuthHeaders($request);
+    if (!$authentication) {
+        $response->getBody()->write(json_encode(["error" => "Invalid authentication options"]));
+        header("Content-Type: application/json");
+        return $response;
+    }
+    $username = $authentication["username"];
+    $password = $authentication["password"];
+
+    $database = $args["database"];
+    $layout = $args["layout"];
+    $fm = new FileMaker($username, $password, $database, $layout);
+    $fields = $fm->getRowNames();
+
+    $response->getBody()->write(json_encode($fields));
+    header("Content-Type: application/json");
+    return $response;
+});
+
+
+$app->run();
+
+
+/**
+ * Parse the authentication headers from the given request object.
+ *
+ * @param Request $request The request object containing the headers.
+ * @return array|false The parsed authentication headers as an array, or false if the headers could not be parsed.
+ */
+function parseAuthHeaders(Request $request): array|false
+{
+    $headers = @$request->getHeader("X-Authentication-Options");
+
+    // Make sure headers are not empty
+    if (count($headers) < 1) {
+        return false;
+    }
+
+    // Get the first header
+    $headers = $headers[0];
+
+    // Check if the headers is not null
+    if (!$headers) {
+        return false;
+    }
+
+    // Try to parse the headers as json
+    try {
+        return json_decode($headers, true);
+    } catch (Exception $e) {
+        return false;
+    }
 }
