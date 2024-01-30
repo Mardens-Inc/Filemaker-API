51283c0062ad50ddd8d6f82036b5e6d9c705f886
drew-chase <dcmanproductions@gmail.com>
2024-01-30 12:46:52 -0500

    Add FilemakerMemory singleton class
    This commit introduces a new class, FilemakerMemory, for managing database tokens within the application. This class uses a singleton pattern to ensure only one instance is created throughout the application. It includes methods for saving, retrieving, deleting, and checking the existence of database tokens.

---
+++ inc/FilemakerMemory.php
@@ -0,0 +1,77 @@
+<?php
+
+
+namespace Filemaker;
+/**
+ * Class FilemakerMemory
+ *
+ * This class represents a memory object for storing database tokens.
+ * It is implemented as a singleton, ensuring that only one instance
+ * of this class is created throughout the application.
+ */
+class FilemakerMemory
+{
+    private static FilemakerMemory $instance;
+    private array $memory;
+
+    protected function __construct()
+    {
+        $this->memory = [];
+    }
+
+    /**
+     * Returns the singleton instance of this class.
+     *
+     * @return FilemakerMemory The singleton instance of this class.
+     */
+    public static function getInstance(): FilemakerMemory
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new FilemakerMemory();
+        }
+        return self::$instance;
+    }
+
+    /**
+     * @param string $database The database to save the token for.
+     * @param string $token The token to save.
+     * @return void
+     */
+    public function save(string $database, string $token): void
+    {
+        $this->memory[$database] = $token;
+    }
+
+
+    /**
+     * Returns the token for the given database.
+     * @param string $database The database to retrieve the token for.
+     * @return string The token for the given database.
+     */
+    public function get(string $database): string
+    {
+        return $this->memory[$database];
+    }
+
+    /**
+     * Returns whether the given database has a token saved.
+     * @param string $database The database to check.
+     * @return bool Whether the given database has a token saved.
+     */
+    public function has(string $database): bool
+    {
+        return isset($this->memory[$database]);
+    }
+
+    /**
+     * Deletes the token for the given database.
+     * @param string $database The database to delete the token for.
+     * @return void
+     */
+    public function delete(string $database): void
+    {
+        unset($this->memory[$database]);
+    }
+
+
+}
