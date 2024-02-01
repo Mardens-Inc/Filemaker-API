31cfa3d1322489abf454de255361d3a85c448c90
drew-chase <dcmanproductions@gmail.com>
2024-02-01 08:32:33 -0500

    Add restUtility requirement and remove parseAuthHeaders function
    A new file restUtility.php has been required in delete.php. The function parseAuthHeaders, which was intended to parse authentication headers from a request, has been removed as it is no longer needed in delete.php.

--- delete.php
+++ delete.php
@@ -9,6 +9,7 @@ use Filemaker\FileMaker;
 require_once './vendor/autoload.php';
 require_once './inc/Filemaker.inc.php';
 require_once './inc/FilemakerMemory.php';
+require_once './restUtility.php';
 
 FilemakerMemory::init();
 
@@ -125,35 +126,3 @@ $app->delete("/database/{database}/layout/{layout}/record/{id}", function (Reque
 
 
 $app->run();
-
-
-/**
- * Parse the authentication headers from the given request object.
- *
- * @param Request $request The request object containing the headers.
- * @return array|false The parsed authentication headers as an array, or false if the headers could not be parsed.
- */
-function parseAuthHeaders(Request $request): array|false
-{
-    $headers = @$request->getHeader("X-Authentication-Options");
-
-    // Make sure headers are not empty
-    if (count($headers) < 1) {
-        return false;
-    }
-
-    // Get the first header
-    $headers = $headers[0];
-
-    // Check if the headers is not null
-    if (!$headers) {
-        return false;
-    }
-
-    // Try to parse the headers as json
-    try {
-        return json_decode($headers, true);
-    } catch (Exception $e) {
-        return false;
-    }
-}
