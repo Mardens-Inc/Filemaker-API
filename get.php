6c2526c828e8f5160c54f9c7290fd48ee2eedb8b
drew-chase <dcmanproductions@gmail.com>
2024-02-01 08:32:45 -0500

    Add restUtility.php import, remove parseAuthHeaders function
    This commit includes import of the restUtility.php file that is required for application functionality. Additionally, it removes the parseAuthHeaders function from get.php file, suggesting a change in approach to parsing authentication headers.

--- get.php
+++ get.php
@@ -10,6 +10,7 @@ use Filemaker\FileMaker;
 require_once './vendor/autoload.php';
 require_once './inc/Filemaker.inc.php';
 require_once './inc/FilemakerMemory.php';
+require_once './restUtility.php';
 
 FilemakerMemory::init();
 
@@ -261,34 +262,3 @@ $app->get("/databases/{database}/layouts/{layout}/search", function (Request $re
 
 $app->run();
 
-
-/**
- * Parse the authentication headers from the given request object.
- *
- * @param Request $request The request object containing the headers.
- * @return array|false The parsed authentication headers as an array, or false if the headers could not be parsed.
- */
-function parseAuthHeaders(Request $request): array|false
-{
-    $headers = @$request->getHeader("X-Authentication-Options");
-
-    // Make sure headers are not empty
-    if (count($headers) < 1) {
-        return false;
-    }
-
-    // Get the first header
-    $headers = $headers[0];
-
-    // Check if the headers is not null
-    if (!$headers) {
-        return false;
-    }
-
-    // Try to parse the headers as json
-    try {
-        return json_decode($headers, true);
-    } catch (Exception $e) {
-        return false;
-    }
-}
